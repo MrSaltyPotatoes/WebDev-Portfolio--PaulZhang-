8abe243f13513ba721a0e7e591970ecf4e8c10de
MrSaltyPotatoes <paulzhang1201@gmail.com>
2025-06-02 15:40:12 +0800

    Delete unnecessary files Update

--- Root Dictionary/Script/test.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Placeholder script loaded!");
-
-    // Example functionality
-    const message = "Hello, this is a placeholder!";
-    alert(message);
-});
