6334690a0af31ec2f167941cfb8901fa5bed20f8
MrSaltyPotatoes <paulzhang1201@gmail.com>
2025-06-02 15:38:56 +0800

    Button Update

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMore() {
+    var moreInfo = document.getElementById("additionalInfo");
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+    } else {
+        moreInfo.style.display = "none";
+    }
+} 
