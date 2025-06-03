e7153bc1e49022a3f807c90ffbf58046163a2fc0
MrSaltyPotatoes <paulzhang1201@gmail.com>
2025-06-03 20:24:54 +0800

    Scroll JavaScript Update

--- scroll.js
+++ scroll.js
@@ -1,4 +1,6 @@
 function scrollToTop() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 } 
